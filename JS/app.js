59546f5633d17d92ef1e95a7b6dfc859f34d6517
VladChappone <chappson50@gmail.com>
2020-12-29 20:03:54 +0000

    complete second landing

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,4 @@
+//slider for quotes
 var slideIndex = 1
 showSlides(slideIndex)
 
@@ -28,11 +29,69 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = 'block'
   dots[slideIndex - 1].className += ' active'
 }
-
+// mobile menu
 function openMobMenu() {
   document.getElementById('mobile-menu').style.width = '100%'
 }
 
 function closeMobMenu() {
   document.getElementById('mobile-menu').style.width = '0%'
+}
+
+// slider for employees
+var employeeIndex = 1
+showEmployees(employeeIndex)
+function plusEmployee(n) {
+  showEmployees((employeeIndex += n))
+}
+
+function showEmployees(n) {
+  var i
+  var employees = document.getElementsByClassName('employees-card-slider')
+
+  if (n > employees.length) {
+    employeeIndex = 1
+  }
+  if (n < 1) {
+    employeeIndex = employees.length
+  }
+  for (i = 0; i < employees.length; i++) {
+    employees[i].style.display = 'none'
+  }
+
+  employees[employeeIndex - 1].style.display = 'block'
+}
+
+// slider for cards
+
+var cardIndex = 1
+showCards(cardIndex)
+
+function plusCard(n) {
+  showCards((cardIndex += n))
+}
+
+function currentCard(n) {
+  showCards((cardIndex = n))
+}
+
+function showCards(n) {
+  var i
+  var cards = document.getElementsByClassName('cards-item-slider')
+  var dots = document.getElementsByClassName('card-dots')
+  if (n > cards.length) {
+    cardIndex = 1
+  }
+  if (n < 1) {
+    cardIndex = cards.length
+  }
+  for (i = 0; i < cards.length; i++) {
+    cards[i].style.display = 'none'
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '')
+  }
+
+  cards[cardIndex - 1].style.display = 'block'
+  dots[cardIndex - 1].className += ' active'
 }
